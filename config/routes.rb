8cc34ae3e1f6d7523e61623842e8403d5f677d48
Jean Jacques MBRA <kouassi.mbra@gmail.com>
2016-10-23 15:06:07 +0200

    From last commit to note displaying

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do 
+    resources :notes, only: [:create, :destroy]
+  end
   root to: "books#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
