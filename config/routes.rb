caeabb197cee320ec440418d313177b6b6e97acf
Jean Jacques MBRA <kouassi.mbra@gmail.com>
2016-10-24 15:22:13 +0200

    Security and pagination

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 
   get "/login" => "sessions#new", as: "login"
 
-  get 'sessions/create'
+  get "sessions/create"
 
-  get "/logout" => "sessions#destroy", as: "logout"
+  delete "/logout" => "sessions#destroy", as: "logout"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Reviewer.destroy_all
 Book.destroy_all
 
 Book.create! [
@@ -14,7 +15,7 @@ Book.create! [
 	{ name: "Design Patterns in Ruby",author: "Russ Olsen" },
 	{ name: "The Ruby Programming Language",author: "David Flanagan" }
 ]
-
+100.times { |index| Book.create! name: "Book#{index}", author: "Author#{index}" }
 eloquent = Book.find_by name: "Eloquent Ruby"
 eloquent.notes.create! [
 	{title: "wow", note: "Great book to learn Ruby"},
